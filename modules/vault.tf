b90ced14fdc1d8497d3a882ba0519f6b56493c96
Jeevaann <121603132+Jeevaann@users.noreply.github.com>
2023-01-09 21:14:15 +0530

    Create vault.tf

---
+++ modules/vault.tf
@@ -0,0 +1,14 @@
+data "vault_aws_access_credentials" "creds" {
+    backend = "aws"
+    role    = "aws-atlantis"
+    type    = "creds"
+}
+  resource "vault_generic_secret" "example" {
+    path = "secret/foo"
+    data_json = jsonencode(
+      {
+        "foo"   = "bar",
+        "pizza" = "cheese"
+      }
+    )
+}
