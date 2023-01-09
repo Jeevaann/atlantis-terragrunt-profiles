793c13ada35b23749916cdbedd9fccddff0368f4
Jeevaann <121603132+Jeevaann@users.noreply.github.com>
2023-01-09 18:38:01 +0530

    Add files via upload

---
+++ modules/vault.tf
@@ -0,0 +1,15 @@
+data "vault_aws_access_credentials" "creds" {
+  backend = "aws"
+  role    = "aws-atlantis"
+  type    = "creds"
+}
+  resource "vault_generic_secret" "example" {
+  path = "secret/foo"
+
+  data_json = jsonencode(
+    {
+      "foo"   = "bar",
+      "pizza" = "cheese"
+    }
+  )
+}
