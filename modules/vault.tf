39d0321decf81db6347f66885e9330d40266c5f1
Jeevaann <121603132+Jeevaann@users.noreply.github.com>
2023-01-10 11:37:35 +0530

    Update vault.tf

--- modules/vault.tf
+++ modules/vault.tf
@@ -1,13 +1,4 @@
-provider "vault" {
-  address = ""
-}
-
-data "vault_aws_access_credentials" "creds" {
-  backend = "aws"
-  role    = "aws-atlantis"
-  type    = "creds"
-}
-  resource "vault_generic_secret" "example" {
+resource "vault_generic_secret" "example" {
   path = "secret/foo"
 
   data_json = jsonencode(
